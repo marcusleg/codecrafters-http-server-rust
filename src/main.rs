439d1d667547981fdb93ba1aa3e8485988597856
Marcus Legendre <marcus.legendre@gmail.com>
2025-05-24 21:29:55 +0200

    Write HTTP response to new TCP connections
    Added a basic HTTP response to incoming connections by utilizing `stream.write_all`. This ensures clients immediately receive an acknowledgment upon connecting. Removed the unused `#[allow(unused_imports)]` directive for cleaner code.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#[allow(unused_imports)]
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -8,7 +8,8 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
