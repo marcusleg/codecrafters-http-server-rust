a982cbc975251b72656b5a003f612272b9364872
Marcus Legendre <marcus.legendre@gmail.com>
2025-05-25 00:44:20 +0200

    Refactor HTTP request handling into a dedicated struct
    Introduce the `HttpRequest` struct to streamline request processing, replacing separate variables for method, path, and headers. This improves readability and makes it easier to pass request data between functions. Updated corresponding handler functions to use the new structure.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,12 @@ struct Args {
     directory: Option<String>,
 }
 
+struct HttpRequest {
+    method: String,
+    path: String,
+    headers: HashMap<String, String>,
+}
+
 static FILES_DIRECTORY: OnceLock<Option<String>> = OnceLock::new();
 
 fn main() {
@@ -36,9 +42,11 @@ fn main() {
 }
 
 fn handle_connection(stream: &mut TcpStream) {
-    let mut method = String::new();
-    let mut path = String::new();
-    let mut headers: HashMap<String, String> = HashMap::new();
+    let mut request = HttpRequest {
+        method: String::new(),
+        path: String::new(),
+        headers: HashMap::new(),
+    };
 
     let reader = BufReader::new(&*stream);
     let mut line_count: usize = 0;
@@ -53,13 +61,15 @@ fn handle_connection(stream: &mut TcpStream) {
                         println!("Invalid HTTP request received.");
                         break;
                     }
-                    method = parts[0].to_string();
-                    path = parts[1].to_string();
-                    println!("Received {} request for {}", method, path);
+                    request.method = parts[0].to_string();
+                    request.path = parts[1].to_string();
+                    println!("Received {} request for {}", request.method, request.path);
                 } else if line.contains(": ") {
                     let parts: Vec<&str> = line.split(": ").collect();
                     if parts.len() == 2 {
-                        headers.insert(parts[0].to_string().to_lowercase(), parts[1].to_string());
+                        request
+                            .headers
+                            .insert(parts[0].to_string().to_lowercase(), parts[1].to_string());
                     }
                     println!("Received header: {}", line)
                 } else {
@@ -74,25 +84,20 @@ fn handle_connection(stream: &mut TcpStream) {
         line_count = line_count + 1;
     }
 
-    handle_request(stream, &method, &path, &headers);
+    handle_request(stream, &request);
 }
 
-fn handle_request(
-    stream: &mut TcpStream,
-    method: &str,
-    path: &str,
-    headers: &HashMap<String, String>,
-) {
-    match method.to_uppercase().as_str() {
+fn handle_request(stream: &mut TcpStream, request: &HttpRequest) {
+    match request.method.to_uppercase().as_str() {
         "GET" => {
-            if path == "/" || path == "/index.html" {
+            if request.path == "/" || request.path == "/index.html" {
                 send_response(stream, 200, None, None);
-            } else if path.starts_with("/echo/") {
-                handle_get_echo(stream, path);
-            } else if path.starts_with("/files/") {
-                handle_get_files(stream, path);
-            } else if path == "/user-agent" {
-                handle_get_user_agent(stream, headers);
+            } else if request.path.starts_with("/echo/") {
+                handle_get_echo(stream, &request);
+            } else if request.path.starts_with("/files/") {
+                handle_get_files(stream, &request);
+            } else if request.path == "/user-agent" {
+                handle_get_user_agent(stream, &request);
             } else {
                 send_response(stream, 404, None, None);
             }
@@ -139,13 +144,13 @@ fn send_response(
     stream.write_all(response.as_bytes()).unwrap();
 }
 
-fn handle_get_echo(stream: &mut TcpStream, path: &str) {
-    let response_body = path.strip_prefix("/echo/").unwrap();
+fn handle_get_echo(stream: &mut TcpStream, request: &HttpRequest) {
+    let response_body = request.path.strip_prefix("/echo/").unwrap();
 
     send_response(stream, 200, Some("text/plain"), Some(response_body));
 }
 
-fn handle_get_files(stream: &mut TcpStream, path: &str) {
+fn handle_get_files(stream: &mut TcpStream, request: &HttpRequest) {
     let files_directory = FILES_DIRECTORY.get().unwrap();
     if files_directory.is_none() {
         send_response(stream, 404, None, None);
@@ -153,7 +158,7 @@ fn handle_get_files(stream: &mut TcpStream, path: &str) {
     }
     let files_directory = files_directory.as_ref().unwrap();
 
-    let file_name = path.strip_prefix("/files/").unwrap();
+    let file_name = request.path.strip_prefix("/files/").unwrap();
     let file_path = format!("{}/{}", files_directory, file_name);
 
     match std::fs::read_to_string(&file_path) {
@@ -175,8 +180,8 @@ fn handle_get_files(stream: &mut TcpStream, path: &str) {
     }
 }
 
-fn handle_get_user_agent(stream: &mut TcpStream, headers: &HashMap<String, String>) {
-    let user_agent = headers.get("user-agent");
+fn handle_get_user_agent(stream: &mut TcpStream, request: &HttpRequest) {
+    let user_agent = request.headers.get("user-agent");
 
     match user_agent {
         None => {
