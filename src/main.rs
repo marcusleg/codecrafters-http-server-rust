cbc24f692e20c822885b55d94a42386f6c5ca408
Marcus Legendre <marcus.legendre@gmail.com>
2025-05-24 22:26:23 +0200

    Handle HTTP request parsing with buffered reader
    Added `BufReader` to parse incoming HTTP requests line by line. This includes logging received lines and handling errors gracefully. Sends a 404 response after processing the request.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
@@ -21,4 +19,23 @@ fn main() {
 
 fn handle_connection(stream: &mut TcpStream) {
     stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+
+    let reader = BufReader::new(&*stream);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                if line.trim().is_empty() {
+                    break;
+                }
+
+                println!("Received: {}", line)
+            }
+            Err(e) => {
+                println!("Failed to read from connection: {}", e);
+                break;
+            }
+        }
+    }
+
+    stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
 }
