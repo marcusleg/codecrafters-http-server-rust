84fa72e377c6b1547d0321a64234514db235a88a
Marcus Legendre <marcus.legendre@gmail.com>
2025-05-24 22:48:35 +0200

    Add basic HTTP request parsing and routing logic
    This update implements parsing of HTTP requests to extract the method and path, as well as preliminary routing logic. It handles GET requests for specific paths and properly responds with 404 or 405 status codes for unsupported paths or methods.

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,56 @@ fn main() {
 }
 
 fn handle_connection(stream: &mut TcpStream) {
-    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+    let mut method = String::new();
+    let mut path = String::new();
 
     let reader = BufReader::new(&*stream);
+    let mut line_count: usize = 0;
     for line in reader.lines() {
         match line {
             Ok(line) => {
                 if line.trim().is_empty() {
                     break;
+                } else if line_count == 0 {
+                    let parts: Vec<&str> = line.split(" ").collect();
+                    if parts.len() < 2 {
+                        println!("Invalid HTTP request received.");
+                        break;
+                    }
+                    method = parts[0].to_string();
+                    path = parts[1].to_string();
+                    println!("Received {} request for {}", method, path);
+                } else if line.contains(": ") {
+                    println!("Received header: {}", line)
+                } else {
+                    println!("Received unknown line: {}", line)
                 }
-
-                println!("Received: {}", line)
             }
             Err(e) => {
                 println!("Failed to read from connection: {}", e);
                 break;
             }
         }
+        line_count = line_count + 1;
     }
 
-    stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+    handle_request(&method, &path, stream);
+}
+
+fn handle_request(method: &str, path: &str, stream: &mut TcpStream) {
+    match method.to_uppercase().as_str() {
+        "GET" => match path {
+            "/index.html" => {
+                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+            }
+            _ => {
+                stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+            }
+        },
+        _ => {
+            stream
+                .write_all(b"HTTP/1.1 405 Method Not Allowed\r\n\r\n")
+                .unwrap();
+        }
+    }
 }
