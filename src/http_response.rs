dc7052a18f8c795d6ddbcb7557cf77d56f378871
Marcus Legendre <marcus.legendre@gmail.com>
2025-05-29 21:03:54 +0200

    Refactor HTTP response sending into modular functions
    Split `send` function into `send_status_line`, `send_headers`, and `send_body` for improved readability and maintainability. Added default content type handling and enhanced error contexts for each step of the response sending process.

--- src/http_response.rs
+++ src/http_response.rs
@@ -12,7 +12,25 @@ pub struct HttpResponse {
 }
 
 pub fn send(stream: &mut TcpStream, mut response: HttpResponse) -> Result<()> {
-    // send status line
+    send_status_line(stream, &mut response)?;
+    send_headers(stream, &mut response)?;
+    send_body(stream, &mut response)?;
+
+    Ok(())
+}
+
+fn send_body(stream: &mut TcpStream, response: &mut HttpResponse) -> Result<()> {
+    match &response.body {
+        None => {}
+        Some(HttpBody::Text(text)) => stream
+            .write_all(text.as_bytes())
+            .context("Failed to send body")?,
+        Some(HttpBody::Binary(bytes)) => stream.write_all(&bytes).context("Failed to send body")?,
+    };
+    Ok(())
+}
+
+fn send_status_line(stream: &mut TcpStream, response: &mut HttpResponse) -> Result<()> {
     let status_line = format!(
         "HTTP/1.1 {} {}\r\n",
         response.status.code, response.status.text
@@ -20,8 +38,11 @@ pub fn send(stream: &mut TcpStream, mut response: HttpResponse) -> Result<()> {
     stream
         .write_all(status_line.as_bytes())
         .context("Failed to send status line")?;
+    Ok(())
+}
 
-    // send headers
+fn send_headers(stream: &mut TcpStream, response: &mut HttpResponse) -> Result<()> {
+    // set default content type
     if response.headers.get("Content-Type").is_none() {
         response
             .headers
@@ -35,6 +56,7 @@ pub fn send(stream: &mut TcpStream, mut response: HttpResponse) -> Result<()> {
             HttpBody::Binary(bytes) => bytes.len(),
         },
     };
+
     response
         .headers
         .insert("Content-Length".to_string(), content_length.to_string());
@@ -45,6 +67,7 @@ pub fn send(stream: &mut TcpStream, mut response: HttpResponse) -> Result<()> {
         .map(|(k, v)| format!("{}: {}\r\n", k, v))
         .collect::<Vec<String>>()
         .join("");
+
     stream
         .write_all(headers_string.as_bytes())
         .context("Failed to send headers")?;
@@ -53,15 +76,6 @@ pub fn send(stream: &mut TcpStream, mut response: HttpResponse) -> Result<()> {
     stream
         .write_all("\r\n".as_bytes())
         .context("Failed to send empty line")?;
-
-    // send body
-    match response.body {
-        None => {}
-        Some(HttpBody::Text(text)) => stream
-            .write_all(text.as_bytes())
-            .context("Failed to send body")?,
-        Some(HttpBody::Binary(bytes)) => stream.write_all(&bytes).context("Failed to send body")?,
-    };
 
     Ok(())
 }
